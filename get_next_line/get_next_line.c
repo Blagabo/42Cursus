24b19a5a6045834495485323674155d8872aa7a4
Gabriel Blanco <65177638+Blagabo@users.noreply.github.com>
2024-02-29 12:09:20 +0000

    Repaso2902

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -24,29 +24,30 @@ char	*get_next_line(int fd)
 	line = NULL;
 	while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
+		printf("bytes_read: %d\n", bytes_read);
 		buffer[bytes_read] = '\0';
 		newline = ft_strchr(buffer, '\n');
 		if (newline != NULL)
 		{
 			line_length = newline - buffer;
-			temp = line;
 			line = malloc(line_length + 1);
 			if (!line)
 				return (NULL);
 			strncpy(line, buffer, line_length);
 			line[line_length] = '\0';
-			if (temp)
+			printf("1 print line: %s\n", line);
+			if (ft_strlen(newline + 1) > 0)
 			{
-				line = realloc(line, line_length + ft_strlen(temp) + 1);
-				if (!line)
+				temp = ft_strdup(newline + 1);
+				if (!temp)
 					return (NULL);
-				strcat(line, temp);
-				free(temp);
+				printf("2 print temp: %s\n", temp);
 			}
 			return (line);
 		}
 		else
 		{
+			printf("ENTRO AL ELSE");
 			if (!line)
 				line = ft_strdup(buffer);
 			else
@@ -68,10 +69,13 @@ int	main(void)
 {
 	int		archivo;
 	char	*linea;
+	char	*linea2;
 
 	archivo = open("test.txt", O_RDONLY);
 	linea = get_next_line(archivo);
-	printf("%s\n", linea);
+	linea2 = get_next_line(archivo);
+	printf("RESULTADO FINAL: %s\n", linea);
+	printf("RESULTADO FINAL2 :%s\n", linea2);
 	close(archivo);
 	return (0);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -11,19 +11,19 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# include <ctype.h>
-# include <fcntl.h>
-# include <limits.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
-# define BUFFER_SIZE 32
+#define GET_NEXT_LINE_H
+#include <ctype.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define BUFFER_SIZE 1000
 
-char	*get_next_line(int fd);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *str);
-size_t	ft_strlen(const char *str);
+char *get_next_line(int fd);
+char *ft_strchr(const char *s, int c);
+char *ft_strdup(const char *str);
+size_t ft_strlen(const char *str);
 
 #endif
